780b0a9dda861f6c39399c6349ec56e168a0fd4c
Unknown <teritry2@gmail.com>
2017-05-09 16:35:06 +0200

    Added datasource for sql

--- regUser.php
+++ regUser.php
@@ -1,3 +1,19 @@
 <?php
-
-
+function createUser()
+{
+    $db = new mysqli("student.cs.hioa.no", "s315584", "", "s31584");
+    if (!$db->connect_error) {
+        die("Feil kunne ikke knytte meg til databasen");
+    }
+    $sql = "Insert Into user (Fornavn, Etternavn, Adresse,";
+    $sql .= "Postnr, Telefonnr) Values ('$this->fornavn',";
+    $sql .= "'$this->etternavn','$this->adresse','$this->postnr'";
+    $sql .= ",'$this->telefonnr')";
+    $resultat = $db->query($sql);
+    if (!$resultat) {
+        return 0;
+    } else {
+        return $db->insert_id;
+    }
+    $db->close();
+}
