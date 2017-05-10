2fa2a19913ade5908674e26a6aee5c68ba23015c
Unknown <teritry2@gmail.com>
2017-05-10 10:46:25 +0200

    Added sql datasorce and made php function to create users

--- regUser.php
+++ regUser.php
@@ -1,14 +1,18 @@
 <?php
-function createUser()
+function createUser($username, $password, $fullname, $email, $phonenr, $address)
 {
+    if($username == null || $password == null || $fullname == null ||$phonenr == null ||$address == null || $email == null){
+        die("Alle datafelt må fylles");
+    }
     $db = new mysqli("student.cs.hioa.no", "s315584", "", "s31584");
     if (!$db->connect_error) {
         die("Feil kunne ikke knytte meg til databasen");
     }
-    $sql = "Insert Into user (Fornavn, Etternavn, Adresse,";
-    $sql .= "Postnr, Telefonnr) Values ('$this->fornavn',";
-    $sql .= "'$this->etternavn','$this->adresse','$this->postnr'";
-    $sql .= ",'$this->telefonnr')";
+    $sql = "Insert Into users (username, password, fullnavn, email, phonenr, address,";
+    $sql.= "Values ('$username','$password','$fullname','$email','$phonenr','$address')";
+  //  $sql .= "Postnr, Telefonnr) Values ('$this->fornavn',";
+  //  $sql .= "'$this->etternavn','$this->adresse','$this->postnr'";
+  //  $sql .= ",'$this->telefonnr')";
     $resultat = $db->query($sql);
     if (!$resultat) {
         return 0;
