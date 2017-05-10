5c7e03428b9ee9e355bd119d7a93ce19cf52a072
Snorre <fotballsnorre@hotmail.com>
2017-05-10 14:55:16 +0200

    Cleaned up unused files

--- RegInfo.php
+++ RegInfo.php
@@ -14,22 +14,24 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
+<nav id="navbar" class="navbar navbar-inverse">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.php">HOME</a>
+            <a class="navbar-brand" href="index.php">HOME </a>
+
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div id="log-head">
-                <ul class="nav navbar-nav navbar-right">
-                    <p class="navbar-text">Signed in as: </p>
-                    <li><button type="button" class="btn btn-success" data-toggle="modal" data-target=".Login">Log In</button></li>
-                    <li><button type="button" class="btn btn-group-justified" onclick="window.location.href='/SkiVM/RegInfo.php'" data-toggle="modal" data-target=".Registrer">Registrer</button></li>
-            </div>
+            <ul class="nav navbar-nav navbar-right" id="loginNav">
+                <p class="navbar-text">Signed in as: </p>
+                <li><button type="button" class="btn btn-success" data-toggle="modal" data-target=".Login">Log In</button></li>
+                <li><button id="regBtn" type="submit" class="btn btn-default" onclick="window.location.href='/SkiVM/RegInfo.php'" data-toggle="modal" data-target=".Registrer" >Registrer</button></li>
+                <li><a id="admin" href="admin.php">Admin</a></li>
             </ul>
+
+
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
--- admin.php
+++ admin.php
@@ -11,22 +11,24 @@
 </head>
 <body>
 
-<nav class="navbar navbar-inverse">
+<nav id="navbar" class="navbar navbar-inverse">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <a class="navbar-brand" href="index.php">HOME</a>
+            <a class="navbar-brand" href="index.php">HOME </a>
+
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <div id="log-head">
-                <ul class="nav navbar-nav navbar-right">
-                    <p class="navbar-text">Signed in as: </p>
-                    <li><button type="button" class="btn btn-success" data-toggle="modal" data-target=".Login">Log In</button></li>
-                    <li><button type="button" class="btn btn-group-justified" onclick="window.location.href='/SkiVM/RegInfo.php'" data-toggle="modal" data-target=".Registrer">Registrer</button></li>
-            </div>
+            <ul class="nav navbar-nav navbar-right" id="loginNav">
+                <p class="navbar-text">Signed in as: </p>
+                <li><button type="button" class="btn btn-success" data-toggle="modal" data-target=".Login">Log In</button></li>
+                <li><button id="regBtn" type="submit" class="btn btn-default" onclick="window.location.href='/SkiVM/RegInfo.php'" data-toggle="modal" data-target=".Registrer" >Registrer</button></li>
+                <li><a id="admin" href="admin.php">Admin</a></li>
             </ul>
+
+
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
@@ -50,7 +52,41 @@
         </div>
     </div>
 <!--Innhold i admin -->
+    <h1 id="tittleAdmin">Admin Konsoll</h1>
+    <div id="tabellBrukere">
+    <table class="table" id="table-props">
+        <thead class="thead-inverse">
+        <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
 </article>
+
 <script src="dist/js/bootstrap.js"></script>
 
 
