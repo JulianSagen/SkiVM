f2ed730a00630d61fb1945fd9b5e6398856d7a12
codekillermuffin <s315594@stud.hioa.no>
2017-05-11 16:25:38 +0200

    dynamic nav bar

--- Registrer.php
+++ Registrer.php
@@ -14,27 +14,10 @@
 </head>
 <body>
 
-<nav id="navbar" class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <a class="navbar-brand" href="index.php">HOME </a>
+<?php
+include_once('navbar.php');
+?>
 
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right" id="loginNav">
-                <p class="navbar-text">Signed in as: </p>
-                <li><button type="button" class="btn btn-success" data-toggle="modal" data-target=".Login">Log In</button></li>
-                <li><button id="regBtn" type="submit" class="btn btn-default" onclick="window.location.href='/Registrer.php'" data-toggle="modal" data-target=".Registrer" >Registrer</button></li>
-                <li><a id="admin" href="admin.php">Admin</a></li>
-            </ul>
-
-
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
 
 <article id="regart">
     <!-- Log in Modal -->
