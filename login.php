963b97702abc16e45a6a03b62ad872f5d9c28607
Unknown <teritry2@gmail.com>
2017-05-12 13:35:17 +0200

    a

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     $username = $_GET["username"];
     $password = $_GET["password"];
 
-    if(checklogin($username, $password) == "isAdmin" || checklogin($username, $password) == "isAdmin"){
+    if(!empty(checklogin($username, $password))){
         echo "Gratulerer du har nå blitt innlogget";
         if(checklogin($username, $password) == "isAdmin") {
             $_SESSION['user_role'] = "admin";
